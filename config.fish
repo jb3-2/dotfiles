784403cd520933af9f1b7ffb076bd3468420a39b
Jonas Bieri <jonas.bieri@gmail.com>
2015-11-13 17:52:43 +0100

    Adjust to DaMac

--- config.fish
+++ config.fish
@@ -5,25 +5,12 @@ set fish_plugins git
 
 # Update the PATH to ensure Homebrew packages are used.
 set homebrew  "/usr/local/bin"
-set netzwerg  "$HOME/bin"
-set dev "$HOME/Dev"
-
-set -gx EDITOR "vim"
-set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home"
-set -gx SCALA_HOME "$dev/scala-2.11.1"
-
-set -gx DOCKER_HOST "tcp://192.168.59.103:2376"
-set -gx DOCKER_CERT_PATH "/Users/netzwerg/.boot2docker/certs/boot2docker-vm"
-set -gx DOCKER_TLS_VERIFY "1"
-
-set -gx PATH $netzwerg $homebrew $SCALA_HOME/bin $XNAT_HOME/bin $PATH
+set -gx PATH $homebrew $PATH
 
 # Handy aliases
-alias e=mvim
-alias mi=mvim
 alias ll='ls -alh'
-alias st=stree
 alias find-note='mdfind -name notes.txt -onlyin ~ -0 | xargs -0 grep -i'
+alias vi=vim
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
